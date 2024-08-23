4018bbb0c6c2c5c70b6c9eb0aff3da2fc7c066b4
igromanru <igromanru@yahoo.de>
2024-08-23 09:06:38 +0200

    Init

---
+++ scripts/main.lua
@@ -0,0 +1,20 @@
+--[[
+    Author: Igromanru
+    Date: 23.08.2024
+    Mod Name: Cheat Console Commands
+]]
+
+------------------------------
+-- Don't change code below --
+------------------------------
+local AFUtils = require("AFUtils.AFUtils")
+require("AFUtils.AFUtilsDebug")
+
+ModName = "CheatConsoleCommands"
+ModVersion = "1.0.0"
+DebugMode = true
+
+LogInfo("Starting mod initialization")
+
+
+LogInfo("Mod loaded successfully")
