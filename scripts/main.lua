f8c0c4b229d37790ac43c64953281cc8e8f90a63
igromanru <igromanru@yahoo.de>
2025-02-05 20:45:43 +0100

    fix: AFUtils.TeleportPlayerToPlayer function

--- scripts/main.lua
+++ scripts/main.lua
@@ -11,7 +11,7 @@ local AFUtils = require("AFUtils.AFUtils")
 local UEHelpers = require("UEHelpers")
 
 ModName = "CheatConsoleCommands"
-ModVersion = "1.17.1"
+ModVersion = "1.17.2"
 DebugMode = true
 IsModEnabled = true
 
