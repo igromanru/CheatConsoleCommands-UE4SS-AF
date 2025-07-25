291905a630e905381ceeef633322a8cccbdf869e
igromanru <igromanru@yahoo.de>
2025-07-25 09:54:55 +0200

    chore: Change to v1.14.1

--- scripts/main.lua
+++ scripts/main.lua
@@ -11,7 +11,7 @@ local AFUtils = require("AFUtils.AFUtils")
 local UEHelpers = require("UEHelpers")
 
 ModName = "CheatConsoleCommands"
-ModVersion = "1.25.0"
+ModVersion = "1.24.1"
 DebugMode = true
 IsModEnabled = true
 
