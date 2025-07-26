88c84fbc779fd3cf73bb6b4cb3c6a91c7fb6ce7c
igromanru <igromanru@yahoo.de>
2025-07-26 19:53:57 +0200

    feat: Improve NoClip

--- scripts/main.lua
+++ scripts/main.lua
@@ -11,7 +11,7 @@ local AFUtils = require("AFUtils.AFUtils")
 local UEHelpers = require("UEHelpers")
 
 ModName = "CheatConsoleCommands"
-ModVersion = "1.25.0"
+ModVersion = "1.25.1"
 DebugMode = true
 IsModEnabled = true
 
